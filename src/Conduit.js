5674787cd6e0797f3b65a7597df0570fe3a881c2
Ben Bonnet <benjamin.a.bonnet@gmail.com>
2025-12-20 10:57:38 -0500

    local before merge with the latest release

--- src/Conduit.js
+++ src/Conduit.js
@@ -349,7 +349,7 @@ function Conduit() {
             }
             // get the cookie and set the auth header
             setSourcererLoading(true);
-            let res = await axios.post(`${config.apiBaseUrl}/sourcerer/`, payload, {
+            let res = await axios.post(`${config.apiBaseUrl}/sourcerer`, payload, {
                 withCredentials:true
             });
     
